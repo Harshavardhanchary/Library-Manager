85acbb51e695fd1697a5a86dec20289714ce4011
Harshavardhanchary <harshavardhanchary7@gmail.com>
2026-01-15 12:04:38 +0530

    edit files

--- terraform/alb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {}
-variable "vpc_id" {}
-variable "public_subnets" {}
-variable "container_port" {
-  default = 8080
-}
-variable "health_check_path" {
-  default = "/Homepage.jsp"
-}
